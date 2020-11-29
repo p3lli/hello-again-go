68baf7f1c209a091c800d08d9ede45963b294ec0
p3lli <p3lli@email.it>
2020-11-29 21:15:15 +0100

    Managed to automatically remove builder image

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 IMAGE_NAME=hello-again-go
 DOCKERFILE=docker-files/Dockerfile-multistage
 TAG=`date +%Y%m%d%H%M%S`
+BUILD_ID=intermediate_builder
 LOCAL_IMAGE="${IMAGE_NAME}:${TAG}"
-docker build -f ${DOCKERFILE} -t ${LOCAL_IMAGE} .
+docker build --build-arg BUILD_ID=${BUILD_ID} -f ${DOCKERFILE} -t ${LOCAL_IMAGE} .
 if test $? -eq 0
 then
     printf "Build done! You can now start the container as follow:\ndocker run -p 8080:8080 ${LOCAL_IMAGE}\n"
+    docker image prune --force --filter label=build=${BUILD_ID}
 else
     printf "ERROR building docker image!\n"
 fi
